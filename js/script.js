58a27665b233bf867efe24f8a8a6ec8225130950
Franco Matias Rossi <95671766+frannqito@users.noreply.github.com>
2023-05-17 15:45:06 -0300

    encriptar

--- js/script.js
+++ js/script.js
@@ -39,31 +39,39 @@ btnDesencriptar.addEventListener("click", () => {
 	});
 });
 
+// function encriptar(texto) {
+// 	let textoEncriptado = "";
+// 	for (let i = 0; i < texto.length; i++) {
+// 		switch (texto[i]) {
+// 			case "e":
+// 				textoEncriptado += "enter";
+// 				break;
+// 			case "i":
+// 				textoEncriptado += "imes";
+// 				break;
+// 			case "a":
+// 				textoEncriptado += "ai";
+// 				break;
+// 			case "o":
+// 				textoEncriptado += "ober";
+// 				break;
+// 			case "u":
+// 				textoEncriptado += "ufat";
+// 				break;
+// 			default:
+// 				textoEncriptado += texto[i];
+// 				break;
+// 		}
+// 	}
+// 	return textoEncriptado;
+// }
 function encriptar(texto) {
-	let textoEncriptado = "";
-	for (let i = 0; i < texto.length; i++) {
-		switch (texto[i]) {
-			case "e":
-				textoEncriptado += "enter";
-				break;
-			case "i":
-				textoEncriptado += "imes";
-				break;
-			case "a":
-				textoEncriptado += "ai";
-				break;
-			case "o":
-				textoEncriptado += "ober";
-				break;
-			case "u":
-				textoEncriptado += "ufat";
-				break;
-			default:
-				textoEncriptado += texto[i];
-				break;
-		}
-	}
-	return textoEncriptado;
+	texto = texto.replaceAll("e", "enter");
+	texto = texto.replaceAll("i", "imes");
+	texto = texto.replaceAll("a", "ai");
+	texto = texto.replaceAll("o", "ober");
+	texto = texto.replaceAll("u", "ufat");
+	return texto;
 }
 
 function desencriptar(texto) {
